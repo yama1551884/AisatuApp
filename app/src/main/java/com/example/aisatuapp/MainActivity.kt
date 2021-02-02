05d55e69aa31b88c0ed5ea1053457d940210b0bd
yama1551884 <yama1551884@gmail.com>
2021-02-02 21:21:30 +0900

    タイマーの時間によってTextViewが変わらない部分を修正しました。

--- app/src/main/java/com/example/aisatuapp/MainActivity.kt
+++ app/src/main/java/com/example/aisatuapp/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.example.aisatuapp
 
 import android.app.TimePickerDialog
-import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -23,9 +22,9 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         val timePickerDialog = TimePickerDialog(
             this,
             TimePickerDialog.OnTimeSetListener { view, hour, minute ->
-                if (hour <= 2 && hour < 10) {
+                if (hour >= 2 && hour < 10) {
                     textView.text = "おはよう"
-                } else if (hour <= 10 && hour < 18) {
+                } else if (hour >= 10 && hour < 18) {
                     textView.text = "こんにちは"
                 } else {
                     textView.text = "こんばんは"
